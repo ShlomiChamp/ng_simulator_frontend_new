12fa45de3f5bbe64f63f28bd7ba455d6678f043c
shlomio <shlomioved01@gmail.com>
2025-01-08 18:35:26 +0200

    Migrated project to standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Added import for RouterModule
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule], // Added RouterModule to imports
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'project-name';
+  title = 'ng_simulator_frontend_new';
 }
